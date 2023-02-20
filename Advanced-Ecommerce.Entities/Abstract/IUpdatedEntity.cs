7dc658510bb19f3041869977c147c1a46c4bdb84
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:35:17 +0300

    IUpdatedEntity was created

---
+++ Advanced-Ecommerce.Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+
+        public int? UpdatedUserId { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
