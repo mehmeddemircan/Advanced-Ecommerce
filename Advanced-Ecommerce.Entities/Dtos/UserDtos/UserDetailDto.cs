5e8c77a986365cf3cc584513845b54a013860b68
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:37:06 +0300

    UserDetailDto was created

---
+++ Advanced-Ecommerce.Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -0,0 +1,34 @@
+﻿using Advanced_Ecommerce.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Dtos.UserDtos
+{   
+
+    //müsteriye göstermek için
+    public class UserDetailDto : IDto
+    {
+
+    
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        //Adress can be different out of that 
+        public string Address { get; set; }
+
+
+
+    }
+}
