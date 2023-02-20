e7a9ffd049c318e784c8a9c9ec57ad48ac39aceb
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:36:55 +0300

    UserAddDto was created

---
+++ Advanced-Ecommerce.Entities/Dtos/UserDtos/UserAddDto.cs
@@ -0,0 +1,32 @@
+﻿using Advanced_Ecommerce.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Dtos.UserDtos
+{
+    public class UserAddDto : IDto
+    {
+
+  
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        //Adress can be different out of that 
+        public string Address { get; set; }
+
+
+
+    }
+}
