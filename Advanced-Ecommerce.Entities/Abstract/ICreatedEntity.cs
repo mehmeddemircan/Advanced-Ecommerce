8a0b440159931080e3bc1b93db6bc983eae721d6
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:34:03 +0300

    ICreatedEntity was created in Entities Abstract

---
+++ Advanced-Ecommerce.Entities/Abstract/ICreatedEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Abstract
+{
+    public interface ICreatedEntity
+    {
+
+        public int CreatedUserId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
