a75707ee89b7edefa63a39ceb7b3c73ae5b88b79
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:35:03 +0300

    IEntity was created

---
+++ Advanced-Ecommerce.Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Abstract
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
