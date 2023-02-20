c43bf55f477bce2d18bd9097734c6220d8096480
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:36:33 +0300

    BaseEntity was Created

---
+++ Advanced-Ecommerce.Entities/Concrete/BaseEntities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using Advanced_Ecommerce.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Concrete.BaseEntities
+{
+    public class BaseEntity : IEntity
+    {
+    }
+}
