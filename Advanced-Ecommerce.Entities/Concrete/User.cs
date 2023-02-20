515ce6e8b6b07d53ad1b3c82c6723d71e0109da7
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:36:42 +0300

    User entity was created

---
+++ Advanced-Ecommerce.Entities/Concrete/User.cs
@@ -0,0 +1,30 @@
+﻿using Advanced_Ecommerce.Entities.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Concrete
+{
+    public class User  : AuditableEntity
+    {
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        //Adress can be different out of that 
+        public string Address { get; set; }
+    }
+}
