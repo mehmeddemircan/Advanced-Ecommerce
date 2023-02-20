88d84926566abb3a50c2c89611d2818e29ab8385
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:36:20 +0300

    AuditableEntity was created

---
+++ Advanced-Ecommerce.Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using Advanced_Ecommerce.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+
+        public int CreatedUserId { get; set; }
+        public DateTime CreatedDate { get  ; set; }
+        public int? UpdatedUserId { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+
+    }
+}
