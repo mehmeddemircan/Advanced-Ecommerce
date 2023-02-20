4749c86818738c753169fbec25fdb22a1cc3bc67
mehmet demircan <mehmeddemircan@gmail.com>
2023-02-20 16:34:37 +0300

    IDisplayEntity was Created

---
+++ Advanced-Ecommerce.Entities/Abstract/IDisplayEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Ecommerce.Entities.Abstract
+{
+    public interface IDisplayEntity
+    {
+
+        public int DisplayOrder { get; set; }
+
+        public bool IsDisplay { get; set; }
+    }
+}
